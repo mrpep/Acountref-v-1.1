9592aece355aad822f3a40e6fb3d5016b84f8c04
fbosio <31016649+fbosio@users.noreply.github.com>
2018-01-17 13:54:33 -0300

    More error handling and stuff like that
    Data validation: now "Go!" button does nothing, unless measurement files, inverse filter and output directory are picked.
    
    Error handling: sampling frequency mismatch between measurement files and the inverse filter raises an error dialog.

--- Functions/Tools/GUI_BatchConvolver.m
+++ Functions/Tools/GUI_BatchConvolver.m
@@ -1,5 +1,4 @@
 function varargout = GUI_BatchConvolver(varargin)
-
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -10,17 +9,17 @@ gui_State = struct('gui_Name',       mfilename, ...
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
-
 if nargout
     [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
 else
     gui_mainfcn(gui_State, varargin{:});
 end
 
+
 function GUI_BatchConvolver_OpeningFcn(hObject, eventdata, handles, varargin)
-global openedmeas openedfilt outdir
-openedmeas = 0;
-openedfilt = 0;
+global selectedMeas selectedFlt outdir %flags
+selectedMeas = 0;
+selectedFlt = 0;
 outdir = 0;
 handles.output = hObject;
 
@@ -33,36 +32,45 @@ varargout{1} = handles.output;
 
 % --- Executes on button press in Btn_FilesConvolve.
 function Btn_FilesConvolve_Callback(hObject, eventdata, handles)
-global openedmeas
+global selectedMeas
 [filename_measurements, pathname_measurements] = uigetfile({'*.wav','WAV-files (*.wav)'},'Pick measurement files','MultiSelect', 'on');
 
-handles.filename_meas = filename_measurements;
-handles.pathname_meas = pathname_measurements;
+% Check if at least one measurement file was chosen
+if iscell(filename_measurements) || ischar(filename_measurements)
+    handles.filename_meas = filename_measurements;
+    handles.pathname_meas = pathname_measurements;
+    selectedMeas = 1;
+    guidata(hObject, handles);
+end
 
-openedmeas = 1;
-guidata(hObject, handles);
 
 function Btn_Filter_Callback(hObject, eventdata, handles)
-global openedfilt
+global selectedFlt
 [filename_filter, pathname_filter] = uigetfile({'*.wav','WAV-files (*.wav)'},'Pick filter files');
 
-handles.filename_filt = filename_filter;
-handles.pathname_filt = pathname_filter;
+if filename_filter % Same as above...
+    handles.filename_filt = filename_filter;
+    handles.pathname_filt = pathname_filter;
+    selectedFlt = 1;
+    guidata(hObject, handles);
+end
 
-openedfilt = 1;
-guidata(hObject, handles);
 
 function Btn_Output_Callback(hObject, eventdata, handles)
 global outdir
 handles.folder_output = uigetdir();
-outdir = 1;
-guidata(hObject, handles);
+
+if handles.folder_output % check if the output folder was chosen
+    outdir = 1;
+    guidata(hObject, handles);
+end
+
 
 function Btn_Calc_Callback(hObject, eventdata, handles)
-global openedmeas openedfilt outdir
+global selectedMeas selectedFlt outdir
 
-if (openedmeas && openedfilt && outdir)
-    [inversefilt, fs] = audioread(strcat(handles.pathname_filt, '\', ...
+if (selectedMeas && selectedFlt && outdir)
+    [inversefilt, fsIF] = audioread(strcat(handles.pathname_filt, '\', ...
         handles.filename_filt));
     
     convolversystem = dsp.Convolver('Method','Frequency Domain');
@@ -76,19 +84,24 @@ if (openedmeas && openedfilt && outdir)
             [xi, fs] = audioread(strcat( handles.pathname_meas, '\', ...
                 char(handles.filename_meas(1, i))));
             
-            impulse = step(convolversystem, xi, inversefilt);
-            
-            if (get(handles.Radio_Normalize, 'Value'))
-                impulse = impulse / max(abs(impulse));
-            elseif (get(handles.Radio_Preserve, 'Value'))
-                if i == 1
-                    divisor = max(abs(impulse));
+            if fs == fsIF
+                impulse = step(convolversystem, xi, inversefilt);
+
+                if (get(handles.Radio_Normalize, 'Value'))
+                    impulse = impulse / max(abs(impulse));
+                elseif (get(handles.Radio_Preserve, 'Value'))
+                    if i == 1
+                        divisor = max(abs(impulse));
+                    end
+                    impulse = 0.8*(impulse/divisor);
                 end
-                impulse = 0.8*(impulse/divisor);
+
+                audiowrite(strcat(handles.folder_output, '\', ...
+                    char(handles.filename_meas(1, i))), impulse, fs);
+            else
+                raiseFsMismatch(char(handles.filename_meas(1, i)), fs, handles.filename_filt, fsIF);
+                break
             end
-            
-            audiowrite(strcat(handles.folder_output, '\', ...
-                char(handles.filename_meas(1, i))), impulse, fs);
         end
         close(h)
     else % only one file was chosen
@@ -97,22 +110,33 @@ if (openedmeas && openedfilt && outdir)
         [x, fs] = audioread(strcat( handles.pathname_meas, '\', ...
                 char(handles.filename_meas)));
         
-        impulse = step(convolversystem, x, inversefilt);
-            
-        if (get(handles.Radio_Normalize, 'Value'))
-            impulse = impulse / max(abs(impulse));
-        elseif (get(handles.Radio_Preserve, 'Value'))
-            divisor = max(abs(impulse));
-            impulse = 0.8*(impulse/divisor);
-        end
-        
-        audiowrite(strcat(handles.folder_output, '\', ...
+        if fs == fsIF
+            impulse = step(convolversystem, x, inversefilt);
+
+            if (get(handles.Radio_Normalize, 'Value'))
+                impulse = impulse / max(abs(impulse));
+            elseif (get(handles.Radio_Preserve, 'Value'))
+                divisor = max(abs(impulse));
+                impulse = 0.8*(impulse/divisor);
+            end
+
+            audiowrite(strcat(handles.folder_output, '\', ...
                 char(handles.filename_meas)), impulse, fs);
-        
-        uiwait(msgbox([ ...
-            'Done. It''s named "Batch Convolver" for a reason, you ' ...
-            'know... you can convolve multiple files by holding the ' ...
-            'Ctrl key while picking them.' ...
-        ], 'Convolution done', 'modal'))
+
+            uiwait(msgbox([ ...
+                'Done. It''s named "Batch Convolver" for a reason, you ' ...
+                'know... you can convolve multiple files by holding the ' ...
+                'Ctrl key while picking them.' ...
+                ], 'Convolution done', 'modal'))
+        else
+            raiseFsMismatch(char(handles.filename_meas), fs, handles.filename_filt, fsIF);
+        end
     end
 end
+
+
+function raiseFsMismatch(file1, fs1, file2, fs2)
+    uiwait(msgbox({...
+        [ file1 ' sampling frequency is ' num2str(fs1) ' Hz while ' ] 
+        [ file2 ' sampling frequency is ' num2str(fs2) ' Hz.' ] ...
+    }, 'Sampling frequency mismatch', 'error'));
